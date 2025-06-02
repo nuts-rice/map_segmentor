79b2d5ff16d5e87c290059e1c67d8ca0b97dccde
nuts_rice <red44443@gmail.com>
2025-06-02 15:58:57 -0400

    feat: build sat map render

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,37 @@ enum SegmentLabel {
     Other,
 }
 
-type BbCoords = (f32, f32, f32, f32);
+#[derive(Debug, Clone, Copy)]
+struct BbCoords {
+    x1: f32,
+    y1: f32,
+    x2: f32,
+    y2: f32,
+}
 
 struct Segment {
     coords: BbCoords,
     segment_type: Option<SegmentLabel>,
     confidence: f32,
+}
+
+impl BbCoords {
+    fn new(x1: f32, y1: f32, x2: f32, y2: f32) -> Self {
+        Self { x1, y1, x2, y2 }
+    }
+    fn area(&self) -> f32 {
+        (self.x2 - self.x1) * (self.y2 - self.y1)
+    }
+
+    fn intersection(&self, other: &Self) -> f32 {
+        let x_overlap = (self.x2.min(other.x2) - self.x1.max(other.x1)).max(0.0);
+        let y_overlap = (self.y2.min(other.y2) - self.y1.max(other.y1)).max(0.0);
+        x_overlap * y_overlap
+    }
+
+    fn union(&self, other: &Self) -> f32 {
+        self.area() + other.area() - self.intersection(other)
+    }
 }
 
 struct Segmentor {
@@ -63,52 +88,41 @@ impl Segmentor {
         Ok(classified_segments)
     }
 
-    async fn generate_segments(&mut self, image: &str) -> Result<Vec<Segment>> {
+    //TODO
+    async fn generate_segments(
+        &mut self,
+        img_w: f32,
+        img_h: f32,
+        embeddings: &Array<f32, IxDyn>,
+    ) -> Result<Vec<Segment>> {
         let mut segments: Vec<Segment> = Vec::new();
-        let img = image::open(image).unwrap();
-        let (img_w, img_h) = (img.width() as f32, img.height() as f32);
-        let img_resized = img.resize(1024, 1024, FilterType::CatmullRom);
-        let (resized_width, resized_height) =
-            (img_resized.width() as f32, img_resized.height() as f32);
 
-        // Copy the image pixels to the tensor, normalizing them using mean and standard deviations
-        // for each color channel
-        let mut input = Array::zeros((1, 3, 1024, 1024)).into_dyn();
-        let mean = vec![123.675, 116.28, 103.53];
-        let std = vec![58.395, 57.12, 57.375];
-        for pixel in img_resized.pixels() {
-            let x = pixel.0 as usize;
-            let y = pixel.1 as usize;
-            let [r, g, b, _] = pixel.2.0;
-            input[[0, 0, y, x]] = (r as f32 - mean[0]) / std[0];
-            input[[0, 1, y, x]] = (g as f32 - mean[1]) / std[1];
-            input[[0, 2, y, x]] = (b as f32 - mean[2]) / std[2];
-        }
-
-        // Prepare tensor for the SAM encoder model
-        /*
-                let outputs: SessionOutputs = self.encoder.run(inputs!["images" => TensorRef::from_array_view(&input)?])?;
-                    let mut segment_bbs: Vec<BbCoords> = Vec::new();
-                    for row in outputs.axis_iter(Axis(0)) {
-                        let row: Vec<_> = row.iter().copied().collect();
-                }
-        */
-
-        /*
-        let encoder_outputs = self.encoder.run(input).unwrap();
-        let embeddings = encoder_outputs
-            .get(0).unwrap()
-            .try_extract::<f32>()
-            .ok().unwrap()
-            .view()
-            .t()
-            .reversed_axes()
-            .into_owned();
-        */
-
-        return Ok(segments);
+        Ok(segments)
     }
 
+    // Prepare tensor for the SAM encoder model
+    /*
+            let outputs: SessionOutputs = self.encoder.run(inputs!["images" => TensorRef::from_array_view(&input)?])?;
+                let mut segment_bbs: Vec<BbCoords> = Vec::new();
+                for row in outputs.axis_iter(Axis(0)) {
+                    let row: Vec<_> = row.iter().copied().collect();
+            }
+    */
+
+    /*
+    let encoder_outputs = self.encoder.run(input).unwrap();
+    let embeddings = encoder_outputs
+        .get(0).unwrap()
+        .try_extract::<f32>()
+        .ok().unwrap()
+        .view()
+        .t()
+        .reversed_axes()
+        .into_owned();
+    */
+
+    //TODO:
+    //SESSION TYPED encoder tx img
     pub async fn encode_image(
         &mut self,
         image: &str,
@@ -146,6 +160,7 @@ impl Segmentor {
             .t()
             .reversed_axes()
             .into_owned();
+
         return Some((embeddings, img_w, img_h, resized_width, resized_height));
     }
 
@@ -156,9 +171,11 @@ impl Segmentor {
     }
 }
 //use ort::Result;
-pub fn main() -> Result<()> {
+#[tokio::main]
+pub async fn main() -> Result<()> {
     let img_path = "../Screenshot_2025-05-27_16-39-42.png";
     let mut segmentor = Segmentor::new("./models/sam2.1_large.onnx", 0.5);
+    let _ = segmentor.encode_image(img_path).await;
     map_render::run();
     Ok(())
 }
--- src/map_render.rs
+++ src/map_render.rs
@@ -1,27 +1,176 @@
 use crate::BbCoords;
-
+use eframe::CreationContext;
+use egui::FontDefinitions;
+use galileo::control::{EventPropagation, MouseButton, UserEvent, UserEventHandler};
+use galileo::galileo_types::cartesian::{Point2, Rect};
+use galileo::galileo_types::geo::Crs;
+use galileo::layer::VectorTileLayer;
 use galileo::layer::feature_layer::{FeatureLayer, FeatureLayerOptions};
+use galileo::layer::raster_tile_layer::RasterTileLayer;
 use galileo::layer::raster_tile_layer::RasterTileLayerBuilder;
+use galileo::layer::vector_tile_layer::VectorTileLayerBuilder;
+use galileo::layer::vector_tile_layer::style::VectorTileStyle;
+use galileo::render::text::RustybuzzRasterizer;
+use galileo::render::text::text_service::TextService;
 use galileo::symbol::ImagePointSymbol;
+use galileo::tile_schema::{TileIndex, TileSchema, VerticalDirection};
+use galileo::{Lod, Map, MapBuilder};
+use galileo_egui::EguiMap;
+use galileo_egui::EguiMapState;
+use parking_lot::RwLock;
+use std::sync::Arc;
 
-use galileo::{Map, MapBuilder};
+const MAPTILER_SAT_URL: &str = "https://api.maptiler.com/tiles/satellite-v2/{z}/{x}/{y}.jpg";
+const ORIGIN: galileo::galileo_types::cartesian::Point2 =
+    galileo::galileo_types::cartesian::Point2::new(-20037508.342787, 20037508.342787);
+
 struct ColoredMask {
     width: u32,
     height: u32,
 }
 
-pub fn run() {
-    galileo_egui::init(create_map(), []).expect("failed to initialize");
+struct MapApp {
+    map: EguiMapState,
+    sat_layer: Arc<RwLock<RasterTileLayer>>,
 }
 
-fn create_map() -> Map {
+impl eframe::App for MapApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            EguiMap::new(&mut self.map).show_ui(ui);
+        });
+
+        egui::Window::new("Buttons")
+            .title_bar(false)
+            .show(ctx, |ui| {
+                ui.horizontal(|ui| {
+                    /*                    if ui.button("Default style").clicked() {
+                                            self.set_style(default_style());
+                                        }
+                                        if ui.button("Gray style").clicked() {
+                                            self.set_style(gray_style());
+                                        }
+                    */
+                });
+            });
+    }
+}
+impl MapApp {
+    fn new(
+        map: Map,
+        layer: Arc<RwLock<RasterTileLayer>>,
+        cc: &CreationContext,
+        handler: impl UserEventHandler + 'static,
+    ) -> Self {
+        let fonts = FontDefinitions::default();
+        let provider = RustybuzzRasterizer::default();
+
+        let text_service = TextService::initialize(provider);
+        for font in fonts.font_data.values() {
+            text_service.load_font(Arc::new(font.font.to_vec()));
+        }
+
+        Self {
+            map: EguiMapState::new(
+                map,
+                cc.egui_ctx.clone(),
+                cc.wgpu_render_state.clone().expect("no render state"),
+                [Box::new(handler) as Box<dyn UserEventHandler>],
+            ),
+            sat_layer: layer,
+        }
+    }
+    /*fn set_style(&mut self, style: RasterTileStyle) {
+        let mut layer = self.sat_layer.write();
+        if style != *layer.style() {
+            layer.update_style(style);
+            self.map.request_redraw();
+        }
+    }
+    */
+}
+/*
+fn default_style() -> VectorTileStyle {
+    serde_json::from_str(include_str!("../data/vt_style.json")).expect("invalid style json")
+}
+
+fn gray_style() -> VectorTileStyle {
+    let style_str = r##"
+{
+  "rules": [
+  ],
+  "background": "#ffffffff",
+  "default_symbol": {
+    "line": {
+      "stroke_color": "#000000ff",
+      "width": 0.5
+    },
+    "polygon": {
+      "fill_color": "#999999ff"
+    }
+  }
+}"##;
+    serde_json::from_str(style_str).expect("invalid style json")
+}
+
+*/
+pub fn run() {
+    galileo_egui::init(create_map().expect("Failed to build map"), [])
+        .expect("failed to initialize");
+}
+
+pub fn download_sat_tile() -> Arc<RwLock<RasterTileLayer>> {
+    let api_key = std::env::var("VT_API_KEY")
+        .expect("Set MapTiler API key in VT_API_KEY environment variable");
+    const ORIGIN: Point2 = Point2::new(-20037508.342787, 20037508.342787);
+    const TOP_RESOLUTION: f64 = 156543.03392800014 / 4.0;
+    let mut lods = vec![Lod::new(TOP_RESOLUTION, 0).expect("invalid config")];
+    for i in 1..16 {
+        lods.push(
+            Lod::new(lods[(i - 1) as usize].resolution() / 2.0, i).expect("invalid tile schema"),
+        );
+    }
+
+    let tile_schema = TileSchema {
+        origin: ORIGIN,
+        bounds: Rect::new(
+            -20037508.342787,
+            -20037508.342787,
+            20037508.342787,
+            20037508.342787,
+        ),
+        lods: lods.into_iter().collect(),
+        tile_width: 1024,
+        tile_height: 1024,
+        y_direction: VerticalDirection::TopToBottom,
+        crs: Crs::EPSG3857,
+    };
+    let layer = RasterTileLayerBuilder::new_rest(move |&index: &TileIndex| {
+        MAPTILER_SAT_URL
+            .replace("{z}", &index.z.to_string())
+            .replace("{x}", &index.x.to_string())
+            .replace("{y}", &index.y.to_string())
+            + &format!("?key={}", api_key)
+    })
+    .with_file_cache_checked(".satellite_cache")
+    .with_tile_schema(tile_schema)
+    .build()
+    .expect("failed to create layer");
+    let layer = Arc::new(RwLock::new(layer));
+    layer
+}
+
+fn create_map() -> Result<Map, Box<dyn std::error::Error>> {
+    let sat_layer = download_sat_tile();
+    let sat_layer_clone = sat_layer.clone();
+
     let raster_layer = RasterTileLayerBuilder::new_osm()
         .with_file_cache_checked(".tile_cache")
         .build()
         .expect("failed to create layer");
-    MapBuilder::default()
+    Ok(MapBuilder::default()
         .with_latlon(39.069067, -108.536016)
         .with_resolution(50.0)
-        .with_layer(raster_layer)
-        .build()
+        .with_layer(sat_layer)
+        .build())
 }
